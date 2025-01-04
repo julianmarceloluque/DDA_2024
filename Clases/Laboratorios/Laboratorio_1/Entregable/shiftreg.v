e7e4c8f2a14ac58a5ec03a8d25cb5e2541284b64
Julian Marcelo <85073950+julianmarceloluque@users.noreply.github.com>
2025-01-04 18:29:53 -0300

    Agrego modificaciones en shiftreg

--- Clases/Laboratorios/Laboratorio_1/Entregable/shiftreg.v
+++ Clases/Laboratorios/Laboratorio_1/Entregable/shiftreg.v
@@ -10,34 +10,34 @@ module shiftreg
 );
 
     //Vars
-    reg [NB_LEDS - 1 : 0]   r_ShiftReg;
+    reg [NB_LEDS - 1 : 0]   r_shiftreg;
 
     always @(posedge clock) begin
         if (i_reset) begin
-            r_ShiftReg <= {{NB_LEDS - 1{1'b0}}, 1'b1}; //1; //4'b0001;
+            r_shiftreg <= {{NB_LEDS - 1{1'b0}}, 1'b1}; //1; //4'b0001;
         end
         else if (i_valid) begin
             // Opcion 1
-            r_ShiftReg      <= r_ShiftReg << 1;
-            r_ShiftReg[0]   <= r_ShiftReg[3];
+            r_shiftreg      <= r_shiftreg << 1;
+            r_shiftreg[0]   <= r_shiftreg[3];
 
             // Opcion 2
-            r_ShiftReg[1]    <= r_ShiftReg[0];
-            r_ShiftReg[2]    <= r_ShiftReg[1];
-            r_ShiftReg[3]    <= r_ShiftReg[2];
-            r_ShiftReg[0]    <= r_ShiftReg[3];
+            r_shiftreg[1]    <= r_shiftreg[0];
+            r_shiftreg[2]    <= r_shiftreg[1];
+            r_shiftreg[3]    <= r_shiftreg[2];
+            r_shiftreg[0]    <= r_shiftreg[3];
             
             // Opcion 3
             for (ptr=0; ptr<NB_LEDS-1; ptr=ptr+1) begin
-                r_ShiftReg(ptr + 1) <= r_ShiftReg(ptr);
+                r_shiftreg(ptr + 1) <= r_shiftreg(ptr);
             end
-            r_ShiftReg[0]   <= r_ShiftReg[NB_LEDS-1];
+            r_shiftreg[0]   <= r_shiftreg[NB_LEDS-1];
 
             // Opcion 4
-            r_ShiftReg      <= {r_ShiftReg[2:0],r_ShiftReg[3]};
+            r_shiftreg      <= {r_shiftreg[2:0],r_shiftreg[3]};
         end
         else begin
-            r_ShiftReg      <= r_ShiftReg;
+            r_shiftreg      <= r_shiftreg;
         end
     end
 
